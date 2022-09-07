cfced86df68792cc2e4da152029da5242edcf415
Kolaposki <oshodikolapo@gmail.com>
2022-09-07 18:32:00 +0100

    :sparkles: Create best selling book component

---
+++ src/components/BestSellingBook.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <section id="best-selling" class="leaf-pattern-overlay">
+        <div class="corner-pattern-overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="inner-content">
+                    <div class="product-element">
+                        <div class="grid">
+                            <figure class="products-thumb">
+                                <img src="../assets/images/single-image.jpg" alt="book" class="single-image">
+                            </figure>
+                            <div class="product-entry">
+                                <h2 class="section-title divider">Best Selling Book</h2>
+
+                                <div class="products-content">
+                                    <div class="author-name">By Timbur Hood</div>
+                                    <h3 class="item-title">Birds gonna be happy</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
+                                    <div class="item-price">$ 45.00</div>
+                                    <div class="btn-wrap">
+                                        <a href="#" class="btn-accent-arrow">shop it now <i class="icon icon-ns-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                                <!--description-->
+
+                            </div>
+                        </div>
+                        <!--grid-->
+                    </div>
+                </div>
+                <!--inner-content-->
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+'name':'BestSellingBook',
+}
+</script>
+
+<style>
+
+</style>
