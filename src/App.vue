b69d859da112af06a3cfb9b903136280e1a1e511
Kolaposki <oshodikolapo@gmail.com>
2022-09-07 18:24:10 +0100

    :art: Refactor main app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div>
+
+  <div id="header-wrap" >
+    <header id="header">
+      <div class="container">
+        <div class="inner-content">
+          <div class="grid">
+            <div class="main-logo">
+              <a href="#"><img src="./assets/images/main-logo.png" alt="logo" /></a>
+            </div>
+
+            <nav id="navbar">
+              <div class="main-menu">
+                <ul class="menu-list">
+                  <li class="menu-item active">
+                    <a href="#home" data-effect="Home">Home</a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="#about" class="nav-link" data-effect="About"
+                      >Books</a
+                    >
+                  </li>
+                  <li class="menu-item">
+                    <a href="#pages" class="nav-link" data-effect="Pages"
+                      >Authors</a
+                    >
+                  </li>
+                  <li class="menu-item">
+                    <div class="search-bar">
+                      <a
+                        href="#"
+                        class="search-button search-toggle"
+                        data-selector="#header-wrap"
+                      >
+                        <i class="icon icon-search"></i>
+                        <span>Search</span>
+                      </a>
+                      <form role="search" method="get" class="search-box">
+                        <input
+                          class="search-field text search-input"
+                          placeholder="Search"
+                          type="search"
+                        />
+                      </form>
+                    </div>
+                  </li>
+                </ul>
+
+                <div class="hamburger">
+                  <span class="bar"></span>
+                  <span class="bar"></span>
+                  <span class="bar"></span>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+  <!--header-wrap-->
+
+  <section id="billboard" class="pattern-overlay">
+ 
+ 
+    <div class="main-slider">
+      <div class="slider-item">
+        <div class="banner-content">
+          <h2 class="banner-title">Life of the wild</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu
+            feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut
+            magna velit eleifend. Amet, quis urna, a eu.
+          </p>
+          <div class="btn-wrap">
+            <a href="#" class="btn-outline-accent btn-accent-arrow"
+              >Read More<i class="icon icon-ns-arrow-right"></i
+            ></a>
+          </div>
+        </div>
+        <!--banner-content-->
+        <img src="./assets/images/main-banner1.jpg" alt="banner" class="banner-image" />
+      </div>
+      <!--slider-item-->
+    </div>
+  </section>
+
+  <Authors/>
+
+  <Books/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Authors from "@/components/Authors";
+import Books from "@/components/Books";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  
+  name: "App",
+  components: {Authors,Books},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/css/normalize.css";
+@import "assets/css/vendor.css";
+@import "assets/css/style.css";
 </style>
