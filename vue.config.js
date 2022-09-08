172f39a8a2708edad2202eaa21dd8e2e450c55d6
Kolaposki <oshodikolapo@gmail.com>
2022-09-08 12:28:56 +0100

    :wrench: Refactor vue config

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,20 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    // on Windows you might want to set publicPath: "http://127.0.0.1:8080/" 
-    publicPath: "http://127.0.0.1:8080/",
-    outputDir: './dist/',
+    publicPath: "https://wajesmart-client.netlify.app/",
+    // publicPath: "http://127.0.0.1:8080/",
+    outputDir: "./dist/",
 
-    chainWebpack: config => {
-
+    chainWebpack: (config) => {
         config
-            .plugin('BundleTracker')
-            .use(BundleTracker, [{ filename: './webpack-stats.json' }])
+            .plugin("BundleTracker")
+            .use(BundleTracker, [{ filename: "./webpack-stats.json" }]);
 
-        config.output
-            .filename('bundle.js')
+        config.output.filename("bundle.js");
 
-        config.optimization
-            .splitChunks(false)
+        config.optimization.splitChunks(false);
 
-        config.resolve.alias
-            .set('__STATIC__', 'static')
+        config.resolve.alias.set("__STATIC__", "static");
 
         config.devServer
             // the first 3 lines of the following code have been added to the configuration
@@ -28,17 +24,15 @@ module.exports = {
             .hotOnly(true)
             .watchOptions({ poll: 1000 })
             .https(false)
-            .disableHostCheck(true)
-            // .headers({ "Access-Control-Allow-Origin": ["\*"] })
-
+            .disableHostCheck(true);
+        // .headers({ "Access-Control-Allow-Origin": ["\*"] })
     },
 
     // uncomment before executing 'npm run build'
     css: {
         extract: {
-            filename: 'bundle.css',
-            chunkFilename: 'bundle.css',
+            filename: "bundle.css",
+            chunkFilename: "bundle.css",
         },
-    }
-
+    },
 };
