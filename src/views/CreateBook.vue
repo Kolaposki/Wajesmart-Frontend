916cb25ced4bcc02d3b8282f927f87f780916dd3
Kolaposki <oshodikolapo@gmail.com>
2022-09-08 08:17:37 +0100

    :sparkles: Design create-book view

---
+++ src/views/CreateBook.vue
@@ -0,0 +1,109 @@
+<template>
+  
+<section class="new-book">
+   <h1 class="title">Create book</h1>
+   <form class="book-form row">
+      <div class="form-field col-lg-6">
+         <input id="name" class="input-text js-input" type="text" required>
+         <label class="label" for="name">Title</label>
+      </div>
+      <div class="form-field col-lg-6 ">
+         <input id="email" class="input-text js-input" type="email" required>
+         <label class="label" for="email">ISBN</label>
+      </div>
+      <div class="form-field col-lg-12">
+         <input id="message" class="input-text js-input" type="text" required>
+         <label class="label" for="message">Author's Name</label>
+      </div>
+      <div class="form-field col-lg-12">
+         <input class="submit-btn" type="submit" value="Submit">
+      </div>
+   </form>
+</section>
+
+</template>
+
+<script>
+export default {
+    name: "CreateBook"
+    
+}
+</script>
+
+<style scoped>
+@import "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css";
+
+.new-book {
+  max-width: 800px;
+  margin: 50px auto;
+  position: relative;
+
+}
+.new-book .title {
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 3.2em;
+  line-height: 48px;
+  padding-bottom: 48px;
+     color: #5543ca;
+    background: #5543ca;
+    background: -moz-linear-gradient(left,#f4524d  0%,#5543ca 100%) !important;
+    background: -webkit-linear-gradient(left,#f4524d  0%,#5543ca 100%) !important;
+    background: linear-gradient(to right,#f4524d  0%,#5543ca  100%) !important;
+    -webkit-background-clip: text !important;
+    -webkit-text-fill-color: transparent !important;
+}
+
+.book-form .form-field {
+  position: relative;
+  margin: 32px 0;
+}
+.book-form .input-text {
+  display: block;
+  width: 100%;
+  height: 36px;
+  border-width: 0 0 2px 0;
+  border-color: #5543ca;
+  font-size: 18px;
+  line-height: 26px;
+  font-weight: 400;
+}
+.book-form .input-text:focus {
+  outline: none;
+}
+.book-form .input-text:focus + .label,
+.book-form .input-text.not-empty + .label {
+  -webkit-transform: translateY(-24px);
+          transform: translateY(-24px);
+}
+.book-form .label {
+  position: absolute;
+  left: 20px;
+  bottom: 11px;
+  font-size: 18px;
+  line-height: 26px;
+  font-weight: 400;
+  color: #5543ca;
+  cursor: text;
+  transition: -webkit-transform .2s ease-in-out;
+  transition: transform .2s ease-in-out;
+  transition: transform .2s ease-in-out, 
+  -webkit-transform .2s ease-in-out;
+}
+.book-form .submit-btn {
+  display: inline-block;
+  background-color: #000;
+   background-image: linear-gradient(125deg,#a72879,#064497);
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 16px;
+  padding: 8px 16px;
+  border: none;
+  width:200px;
+  cursor: pointer;
+}
+
+
+</style>
