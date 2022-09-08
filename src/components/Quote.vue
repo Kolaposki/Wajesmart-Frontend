00e32e4483d57edeb3de27b0826b9bb1cff35419
Kolaposki <oshodikolapo@gmail.com>
2022-09-08 02:42:40 +0100

    :sparkles: Create quote component

---
+++ src/components/Quote.vue
@@ -0,0 +1,20 @@
+<template>
+   
+      <section id="quotation" class="align-center">
+        <div class="inner-content">
+            <h2 class="section-title divider">Quote of the day</h2>
+            <blockquote >
+                <q>“The more that you read, the more things you will know. The more that you learn, the more places you’ll go.”</q>
+                <div class="author-name">Dr. Seuss</div>
+            </blockquote>
+        </div>
+    </section>
+
+</template>
+
+<script>
+export default {
+  name: "Quote",
+
+}
+</script>
