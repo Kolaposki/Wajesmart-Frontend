1381dbadcd955c27e74dc2633d3d354f47de0c0d
Kolaposki <oshodikolapo@gmail.com>
2022-09-09 18:46:18 +0100

    :rocket: Implement collapsing of navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,57 @@
 <template>
   <div id="header-wrap">
-   
-      <!-- header section strats -->
+    <!-- header section strats -->
     <header class="header_section">
       <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+          <router-link class="navbar-brand" :to="{ name: 'Home' }">
+            <span>Bookclub</span>
+          </router-link>
 
-          <router-link
-                     class="navbar-brand"
-                      :to="{ name: 'Home' }"
-                    >
-                      <span>Bookclub</span>
-                    </router-link>
-
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            @click="isCollapsed = !isCollapsed"
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class=""> </span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav  ml-auto">
+          <div
+            :class="{ collapse: isCollapsed }"
+            class="navbar-collapse"
+            id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-
-                  <router-link
-                      class="nav-link"
-                      :to="{ name: 'Home' }"
-                    >
-                      Home <span class="sr-only">(current)</span>
-                    </router-link>
-
+                <router-link class="nav-link" :to="{ name: 'Home' }">
+                  Home <span class="sr-only">(current)</span>
+                </router-link>
               </li>
               <li class="nav-item">
-                <router-link
-                      class="nav-link"
-                      :to="{ name: 'Books' }"
-                    >
-                      Books 
-                    </router-link>
+                <router-link class="nav-link" :to="{ name: 'Books' }">
+                  Books
+                </router-link>
               </li>
-             
+
               <li class="nav-item">
                 <router-link class="nav-link" :to="{ name: 'Authors' }">
-                      Authors
-                    </router-link>
+                  Authors
+                </router-link>
               </li>
               <li class="nav-item">
-                 <router-link class="nav-link" :to="{ name: 'CreateBook' }">
-                      New Book
-                    </router-link>
+                <router-link class="nav-link" :to="{ name: 'CreateBook' }">
+                  New Book
+                </router-link>
               </li>
               <li class="nav-item">
-                 <router-link
-                      class="nav-link"
-                      :to="{ name: 'CreateAuthor' }"
-                    >
-                      New Author
-                    </router-link>
+                <router-link class="nav-link" :to="{ name: 'CreateAuthor' }">
+                  New Author
+                </router-link>
               </li>
             </ul>
           </div>
@@ -69,6 +65,16 @@
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      isCollapsed: true,
+    };
+  },
+  watch: {
+    $route() {
+      this.isCollapsed = true;
+    },
+  },
 };
 </script>
 
