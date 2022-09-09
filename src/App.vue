b4ae8a47a90ef14fc1e35eb0663bc3469b83b932
Kolaposki <oshodikolapo@gmail.com>
2022-09-09 18:34:27 +0100

    :art: Refactor code

--- src/App.vue
+++ src/App.vue
@@ -28,13 +28,9 @@ export default {
 
 <style>
 
-
-/* @import "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.3.1/js/bootstrap.min.js"; */
-@import "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.1/js/bootstrap.min.js";
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
-
-@import "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap";
+@import "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;90x0&display=swap";
+@import "assets/css/bootstrap.css";
 @import "assets/css/style.css";
-@import "assets/css/style2.css";
 @import "assets/css/responsive.css";
 </style>
